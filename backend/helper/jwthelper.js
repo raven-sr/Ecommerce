5025c08956d0815108bfbe1d6ae912c372de3220
Siva Sundaram <rjkumar1982kum@gmail.com>
2026-08-19 16:27:49 +0530

    Fix server port for deployment

--- backend/helper/jwthelper.js
+++ backend/helper/jwthelper.js
@@ -2,7 +2,9 @@ const sendToken=(user,statuscode,res)=>{
      const token= user.getJWTToken()
      const props={
         expires: new Date(Date.now()+ process.env.COOKIE_EXPIRE_DATE * 24 * 60 * 60 * 1000),
-        httpOnly:true
+        httpOnly:true,
+        secure: true,
+        sameSite: "none"
      }
   return res.status(statuscode).cookie("token",token,props).json({
         sucess:true,
