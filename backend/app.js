0c8ecc52cd423c7ad5cfe417c48757a5146887db
Siva Sundaram <rjkumar1982kum@gmail.com>
2026-08-19 15:01:59 +0530

    Fix server port for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,35 @@
-import express from "express"
-import productRoutes from "./routes/routes.js"
-import userRoutes from "./routes/userroutes.js"
-import orderRoutes from "./routes/orderroutes.js"
-import cartRoutes from "./routes/cartroute.js"
-import error from "./middleware/middleware.js"
-import CookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+import productRoutes from "./routes/routes.js";
+import userRoutes from "./routes/userroutes.js";
+import orderRoutes from "./routes/orderroutes.js";
+import cartRoutes from "./routes/cartroute.js";
+
+import error from "./middleware/middleware.js";
+import CookieParser from "cookie-parser";
+
+const app = express();
+
+// CORS
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://ecommerce-a2ne0mtjx-sms-11c2.vercel.app"
+    ],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(CookieParser());
 
-app.use("/api/v1/",productRoutes);
-app.use("/api/v1/",userRoutes);
-app.use("/api/v1/",orderRoutes);
+// Routes
+app.use("/api/v1/", productRoutes);
+app.use("/api/v1/", userRoutes);
+app.use("/api/v1/", orderRoutes);
 app.use("/api/v1/", cartRoutes);
 
+// Error middleware
 app.use(error);
 
-
-export default app
+export default app;
